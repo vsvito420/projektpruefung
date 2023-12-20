804b5fd732e9e582920b370f90b4f08cc9119be3
VitoS <ikn0@proton.me>
2023-12-20 22:11:34 +0100

    update

--- provider.tf
+++ provider.tf
@@ -16,16 +16,18 @@ variable "proxmox_api_url" {
 
 variable "proxmox_api_token_id" {
     type = string
+    sensitive = true
 }
 
 variable "proxmox_api_token_secret" {
     type = string
+    sensitive = true
 }
 
 provider "proxmox" {
 
-    pm_api_url = var.proxmox_api_url
-    pm_api_token_id = var.proxmox_api_token_id
+    pm_api_url = var.proxmox_api_url 
+    pm_api_token_id = var.proxmox_api_token_id 
     pm_api_token_secret = var.proxmox_api_token_secret
 
     # (Optional) Skip TLS Verification
