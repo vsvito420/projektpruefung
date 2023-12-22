30d6353cc897db617f4ba77ef81d3a8a33cfa58e
VitoS <ikn0@proton.me>
2023-12-22 12:05:17 -0800

    Update Cloudflare provider configuration

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,25 +1,26 @@
-# Configure the Cloudflare provider using the required_providers stanza
-# required with Terraform 0.13 and beyond. You may optionally use version
-# directive to prevent breaking changes occurring unannounced.
 terraform {
-  required_providers {
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "~> 4.0"
+    required_providers {
+        cloudflare = {
+            source = "cloudflare/cloudflare"
+            version = "~> 4.0"
+        }
     }
-  }
 }
 
-#provider "cloudflare" {
-#  api_token = var.cloudflare_api_token
-#}
+provider "cloudflare" {
+    api_token = var.cloudflare_api_token
+}
 
-# Create a record
-#resource "cloudflare_record" "www" {
-  # ...
-#}
+variable "cloudflare_api_token" {
+    type        = string
+    description = "API token for Cloudflare"
+}
 
-# Create a page rule
-#resource "cloudflare_page_rule" "www" {
-  # ...
-#}
+/*resource "cloudflare_record" "www" {
+    # ...
+}
+
+resource "cloudflare_page_rule" "www" {
+
+}
+*/
