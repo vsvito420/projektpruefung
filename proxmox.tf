e722dfb429c28f01c2bdb48894983e15e276198f
VitoS <ikn0@proton.me>
2024-01-30 18:11:19 +0100

    !

--- proxmox.tf
+++ proxmox.tf
@@ -1,15 +1,3 @@
-terraform {
-
-    required_version = ">= 1.5.1"
-
-    required_providers {
-        proxmox = {
-            source = "telmate/proxmox"
-            version = "2.9.14"
-        }
-    }
-}
-
 variable "proxmox_api_url" {
     type = string
 }
@@ -25,7 +13,6 @@ variable "proxmox_api_token_secret" {
 }
 
 provider "proxmox" {
-
     pm_api_url = var.proxmox_api_url 
     pm_api_token_id = var.proxmox_api_token_id 
     pm_api_token_secret = var.proxmox_api_token_secret
---
+++ terraform.tf
@@ -0,0 +1,15 @@
+terraform {
+
+    required_version = ">= 1.5.1"
+
+    required_providers {
+        proxmox = {
+            source = "telmate/proxmox"
+            version = "3.0.1-rc1"
+        }
+        cloudflare = {
+            source = "cloudflare/cloudflare"
+            version = "~> 4.0"
+        }
+    }
+}
